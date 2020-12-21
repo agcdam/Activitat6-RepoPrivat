30b0a159e240888d20a7365c4c97d101a56c9590
Alonso Gómez Casanova <alonsogomezcasanova@gmail.com>
2020-12-21 17:20:41 +0100

    Añadido metodo numeros

--- Projecto_Java_GitHub/src/ed/alonso/HelloGitHub.java
+++ Projecto_Java_GitHub/src/ed/alonso/HelloGitHub.java
@@ -6,6 +6,20 @@ public class HelloGitHub {
 		
 		System.out.println("Hola GitHub, sóc Alonso Gómez Casanova");
 		
+		System.out.println();
+		
+		numeros();
+		
+	}
+	
+	public static void numeros() {
+		
+		for(int i = 0; i < 20; i++) {
+			
+			System.out.print(i + 1 + " ");
+			
+		}
+		
 	}
 
 }
